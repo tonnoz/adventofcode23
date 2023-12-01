8801394b961255019b434fd3b55fbdfe3485be49
tonnoz <info@tonnoz.com>
2023-12-01 14:02:04 +0100

    first exercise aoc23

---
+++ src/test/kotlin/com/tonnoz/adventofcode23/Adventofcode23ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tonnoz.adventofcode23
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Adventofcode23ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
