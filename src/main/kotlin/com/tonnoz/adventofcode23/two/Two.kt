29a3f3a0af03f48b82b48a794ab0f8cfdb8c57ea
tonnoz <info@tonnoz.com>
2023-12-02 11:03:26 +0100

    problem 2: part 2 solved

--- src/main/kotlin/com/tonnoz/adventofcode23/two/Two.kt
+++ src/main/kotlin/com/tonnoz/adventofcode23/two/Two.kt
@@ -1,20 +1,17 @@
 package com.tonnoz.adventofcode23.two
 
 import com.tonnoz.adventofcode23.one.readFileAsAList
+typealias Color = String
 
-//max 12 red cubes, 13 green cubes, and 14 blue cubes
-data class GameRound(val blueC: Int?, val greenC: Int?, val redC:Int?)
-data class Game(val gameNr: Int, val rounds: List<GameRound>)
+//max 12 red cubes, 13 green cubes, and 14 blue cubes for the first problem
+data class GameRound(val blueCubes: Int?, val greenCubes: Int?, val redCubes:Int?)
+data class Game(val gameNr: Int, val rounds: List<GameRound>, val minCubes: Map<Color, Int>)
 
 private const val SPACE = " "
 private const val SEMICOLON = ";"
 private const val COLON = ":"
 private const val COMMA = ","
-fun GameRound.isInvalid(): Boolean = (this.redC ?: 0) > 12 || (this.greenC ?: 0) > 13 || (this.blueC ?: 0) > 14
-
-operator fun Game.plus(anotherGame: Game): Game {
-  return Game(this.gameNr + anotherGame.gameNr, emptyList())
-}
+fun GameRound.isInvalid(): Boolean = (this.redCubes ?: 0) > 12 || (this.greenCubes ?: 0) > 13 || (this.blueCubes ?: 0) > 14
 
 fun String.toGame(): Game {
   val line = this.split(COLON)
@@ -22,22 +19,36 @@ fun String.toGame(): Game {
   val roundsString = line.last().split(SEMICOLON)
   val rounds = roundsString.map {
     it.split(COMMA)
-      .map { color -> color.trim() }
-      .map { gameThrow ->
-        val (first, second) = gameThrow.split(SPACE)
+      .map { gameThrow -> gameThrow.trim() }
+      .map { gameThrowT ->
+        val (first, second) = gameThrowT.split(SPACE)
         first to second
       }.associate { (first, second) ->
         second to first.toInt()
       }
-  }.map { round ->
-    GameRound(round["blue"], round["green"], round["red"])
-  }
-  return Game(gameNr, rounds)
+  }.map { GameRound(it["blue"], it["green"], it["red"]) }
+  return Game(gameNr, rounds, emptyMap())
+}
+
+fun Game.toGameExtra(): Game {
+  val maxBlue = this.rounds.maxBy { it.blueCubes ?: 0 }.blueCubes ?: 1
+  val maxRed = this.rounds.maxBy { it.redCubes ?: 0 }.redCubes ?: 1
+  val maxGreen = this.rounds.maxBy { it.greenCubes ?: 0 }.greenCubes ?: 1
+  return Game(this.gameNr, this.rounds, mapOf("blue" to maxBlue, "red" to maxRed, "green" to maxGreen))
 }
 
 fun main() {
-  "inputTwo.txt".readFileAsAList().map { lineString -> lineString.toGame() }
-    .filter{ it.rounds.none {gameR ->  gameR.isInvalid() } }
-    .reduce { acc, curr -> acc + curr}
-    .gameNr.let { println(it) } //answer is 2169
+  val input = "inputTwo.txt".readFileAsAList()
+  val games = input.map { it.toGame() }
+
+  games
+    .filter { it.rounds.none { gameR -> gameR.isInvalid() } }
+    .fold(0) { acc, curr -> acc + curr.gameNr}
+    .let { println(it) } //answer first problem is 2169
+
+  games
+    .map { it.toGameExtra() }
+    .fold(0) { acc, curr -> acc + curr.minCubes.values.reduce(Int::times) }
+    .let { println(it) } //answer second problem is 60948
+
 }
