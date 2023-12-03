594974207e3df9772acbab002c1631a99e4a50eb
tonnoz <info@tonnoz.com>
2023-12-03 17:39:16 +0100

    iterative solution for day 3 part 2

---
+++ src/main/kotlin/com/tonnoz/adventofcode23/three/ThreeIter.kt
@@ -0,0 +1,23 @@
+package com.tonnoz.adventofcode23.three
+
+//iterative version of problem two
+//fun problemTwo(input: List<String>) {
+//  var iLine = 0
+//  val resultList = mutableListOf<Int>()
+//  while (iLine < input.size) {
+//    val line = input[iLine]
+//    var iChar = 0
+//    while (iChar < line.length) {
+//      val aChar = line[iChar]
+//      if (aChar.isNonGearSymbol() || aChar.isDigit()) {
+//        iChar++
+//        continue
+//      }
+//      println("iLine: $iLine, iChar: $iChar")
+//      resultList.add(input.getGearsProductIfBothPresentOrElseZero(iLine, iChar))
+//      iChar++
+//    }
+//    iLine++
+//  }
+//  println("the solution to problem two is: ${resultList.sum()}") //84900879
+//}
