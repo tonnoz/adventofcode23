8b2036be2dc76d340f9b5dd0ef3dcd49f2ee8e70
tonnoz <info@tonnoz.com>
2023-12-03 17:32:41 +0100

    problem three part 2 solved

--- src/main/kotlin/com/tonnoz/adventofcode23/three/Three.kt
+++ src/main/kotlin/com/tonnoz/adventofcode23/three/Three.kt
@@ -4,11 +4,75 @@ import com.tonnoz.adventofcode23.one.readFileAsAList
 
 val symbols = hashSetOf('@','#', '$', '%', '&', '*', '+', '-', '=', '/')
 
-
 fun main() {
   val input = "inputThree.txt".readFileAsAList()
   problemOne(input)
+  problemTwo(input)
 }
+
+fun problemTwo(input: List<String>) {
+  input.flatMapIndexed { iLine, line ->
+    line.mapIndexedNotNull { iChar, aChar ->
+      if (aChar == '*') input.getGearsProductIfBothPresentOrElseZero(iLine, iChar)
+      else null
+    }
+  }.sum().let {
+    println("the solution to problem two is: $it") //84900879
+  }
+}
+private fun List<String>.getGearsProductIfBothPresentOrElseZero(iLine: Int, iChar: Int): Int =
+  hashSetOf(
+    getGearNumberLeft(iLine, iChar),
+    getGearNumberRight(iLine, iChar),
+    getGearNumberTopCenter(iLine, iChar),
+    getGearNumberTopLeft(iLine, iChar),
+    getGearNumberTopRight(iLine, iChar),
+    getGearNumberBottomCenter(iLine, iChar),
+    getGearNumberBottomLeft(iLine, iChar),
+    getGearNumberBottomRight(iLine, iChar)
+  ).let {
+    it.remove(0)
+    if (it.size != 2) return 0
+    return it.reduce { acc, curr -> acc * curr }
+  }
+
+private fun List<String>.getGearNumberRight(iLine: Int, iChar: Int) = this[iLine].findNumber(iChar + 1).tryToIntOrZero()
+private fun List<String>.getGearNumberLeft(iLine: Int, iChar: Int) = this[iLine].findNumber(iChar - 1).tryToIntOrZero()
+fun String.tryToIntOrZero(): Int = try { this.toInt() } catch (e: Exception) { 0 }
+fun String.findNumber(iChar: Int): String {
+  if (iChar !in this.indices || !this[iChar].isDigit()) return ""
+  val startIndex = this.substring(0, iChar).indexOfLast { !it.isDigit() } + 1
+  val lastIndexOfADigit = this.substring(iChar).indexOfFirst { !it.isDigit() }
+  val endIndex = if(lastIndexOfADigit == -1) this.length else this.substring(iChar).indexOfFirst { !it.isDigit() } + iChar
+  return if (endIndex > -1) {
+    this.substring(startIndex, endIndex)
+  } else {
+    this.substring(startIndex)
+  }
+}
+
+fun List<String>.getGearNumberTopCenter(iLine: Int, iChar: Int): Int = this.getNumber(iLine, iChar, -1,0)
+fun List<String>.getGearNumberTopLeft(iLine: Int, iChar: Int): Int = this.getNumber(iLine, iChar, -1,-1)
+fun List<String>.getGearNumberTopRight(iLine: Int, iChar: Int): Int = this.getNumber(iLine, iChar, -1,1)
+fun List<String>.getGearNumberBottomCenter(iLine: Int, iChar: Int): Int = this.getNumber(iLine, iChar, +1,0)
+fun List<String>.getGearNumberBottomLeft(iLine: Int, iChar: Int): Int  = this.getNumber(iLine, iChar, +1,-1)
+fun List<String>.getGearNumberBottomRight(iLine: Int, iChar: Int): Int  = this.getNumber(iLine, iChar, +1,1)
+fun List<String>.getNumber(iLine:Int, iChar:Int, lineIAddendum: Int, charIAddendum: Int): Int {
+  if(iLine == 0) return 0
+  if(iLine == this.size - 1) return 0
+  if(!this[iLine + lineIAddendum][iChar + charIAddendum].isDigit()) return 0
+  return this[iLine + lineIAddendum].findNumber(iChar + charIAddendum).tryToIntOrZero()
+}
+
+
+
+
+
+
+
+
+
+//****************************** PROBLEM ONE ******************************//
 
 private fun problemOne(input: List<String>) {
   var iLine = 0
@@ -22,8 +86,8 @@ private fun problemOne(input: List<String>) {
         iChar++
         continue
       }
-      if (aChar.isACandidate(input, iLine, iChar)) {
-        val aNumber = getNextNumber(line, iChar)
+      if (isACandidate(input, iLine, iChar)) {
+        val aNumber = line.getNextNumberRightOf(iChar)
         iChar += aNumber.length
         resultList.add(aNumber.toInt())
         continue
@@ -32,18 +96,17 @@ private fun problemOne(input: List<String>) {
     }
     iLine++
   }
-  println(resultList.sum())
+  println("the result of first problem is ${resultList.sum()}") // 530849
 }
 
 private fun Char.isASymbol(): Boolean = symbols.contains(this)
 
-
-private fun Char.isACandidate(input: List<String>, iLine: Int, iChar: Int): Boolean {
-  val aNumber = getNextNumber(input[iLine], iChar)
-  return checkNumberAdjacentH(aNumber, input[iLine], iChar) || checkNumberAdjacentVorD(aNumber, input, iLine, iChar, input[iLine])
+private fun isACandidate(input: List<String>, iLine: Int, iChar: Int): Boolean {
+  val aNumber = input[iLine].getNextNumberRightOf(iChar)
+  return isNumberAdjacentHorizontal(aNumber, input[iLine], iChar) || isNumberAdjacentVerticalOrDiagonal(aNumber, input, iLine, iChar, input[iLine])
 }
 
-fun checkNumberAdjacentVorD(aNumber: String, input: List<String>, iLine: Int, iChar: Int, line: String): Boolean {
+fun isNumberAdjacentVerticalOrDiagonal(aNumber: String, input: List<String>, iLine: Int, iChar: Int, line: String): Boolean {
   val skipTop = iLine == 0
   val skipBottom = iLine == input.size - 1
   var i = if(iChar>0) iChar-1 else iChar
@@ -56,9 +119,8 @@ fun checkNumberAdjacentVorD(aNumber: String, input: List<String>, iLine: Int, iC
   return false
 }
 
-fun checkNumberAdjacentH(aNumber: String, line: String, iChar: Int): Boolean =
+fun isNumberAdjacentHorizontal(aNumber: String, line: String, iChar: Int): Boolean =
   (iChar-1 >= 0 && line[iChar-1].isASymbol()) || (iChar + aNumber.length < line.length && line[iChar + aNumber.length].isASymbol())
 
 
-fun getNextNumber(line: String, iChar: Int): String = line.substring(iChar).takeWhile { it.isDigit() }
-
+fun String.getNextNumberRightOf(iChar: Int): String = this.substring(iChar).takeWhile { it.isDigit() }
