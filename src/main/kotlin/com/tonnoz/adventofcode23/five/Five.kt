fdf506c14134901e544c956b2167546b4d8d20e8
tonnoz <info@tonnoz.com>
2023-12-05 18:44:35 +0100

    Day 5 part 1 completed

---
+++ src/main/kotlin/com/tonnoz/adventofcode23/five/Five.kt
@@ -0,0 +1,62 @@
+package com.tonnoz.adventofcode23.five
+
+import com.tonnoz.adventofcode23.one.readFileAsAList
+
+object Five {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val time = System.currentTimeMillis()
+    val input = "inputFive.txt".readFileAsAList()
+    val seeds = input[0].parseSeeds()
+    val seedToSoilMap = input.parseRanges("seed-to-soil map:", "")
+    val soilToFertilizerMap = input.parseRanges("soil-to-fertilizer map:", "")
+    val fertilizerToWaterMap = input.parseRanges("fertilizer-to-water map:", "")
+    val waterToLightMap = input.parseRanges("water-to-light map:", "")
+    val lightToTemperatureMap = input.parseRanges("light-to-temperature map:", "")
+    val temperatureToHumidityMap = input.parseRanges("temperature-to-humidity map:", "")
+    val humidityToLocationMap = input.parseRanges("humidity-to-location map:", "")
+    seeds.asSequence()
+      .map { checkSeedAgainstRanges(it,seedToSoilMap) }
+      .map { checkSeedAgainstRanges(it,soilToFertilizerMap) }
+      .map { checkSeedAgainstRanges(it,fertilizerToWaterMap) }
+      .map { checkSeedAgainstRanges(it,waterToLightMap) }
+      .map { checkSeedAgainstRanges(it,lightToTemperatureMap) }
+      .map { checkSeedAgainstRanges(it,temperatureToHumidityMap) }
+      .map { checkSeedAgainstRanges(it,humidityToLocationMap) }
+      .min()
+      .let { println("lowest location: $it") }
+    println("seconds elapsed: ${(System.currentTimeMillis() - time) / 1000}")
+  }
+
+  private data class Range(val sourceRange: LongRange, val destinationRange: LongRange)
+
+  private fun String.parseSeeds() : List<Long> = "\\d+".toRegex().findAll(this).map { it.value.toLong() }.toList()
+
+  private fun checkSeedAgainstRanges(seed: Long, ranges: List<Range>): Long {
+    ranges.forEach {
+      val maybeSeed = transform(seed, it)
+      if(maybeSeed != null) return maybeSeed
+    }
+    return seed
+  }
+
+  private fun transform(seed: Long, range: Range): Long? = range
+    .sourceRange
+    .positionInTheRange(seed)
+    ?.let { range.destinationRange.elementAt(it.toInt()) }
+
+
+  private fun LongRange.positionInTheRange(number: Long): Long? = if (number in this) number - this.first else null
+
+  private fun List<String>.parseRanges(startLine:String, endLine:String): List<Range> {
+    val startI = this.indexOfFirst { it.startsWith(startLine) } + 1
+    val endI = this.subList(startI+1, this.size).indexOfFirst { it == endLine } + startI + 1
+    return this.subList(startI, endI).map {
+      val (destination,source, rangeLength ) = it.split(" ", limit = 3)
+      val sourceRange = source.toLong()..< source.toLong() + rangeLength.toLong()
+      val destinationRange = destination.toLong()..< destination.toLong() + rangeLength.toLong()
+      Range(sourceRange, destinationRange)
+    }
+  }
+}
