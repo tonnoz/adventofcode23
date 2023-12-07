f5106049ada8a9517d3be868c25f36daf274cd96
tonnoz <info@tonnoz.com>
2023-12-07 20:39:28 +0100

    seven part 2 + refactoring

--- src/main/kotlin/com/tonnoz/adventofcode23/seven/Seven.kt
+++ src/main/kotlin/com/tonnoz/adventofcode23/seven/Seven.kt
@@ -9,6 +9,7 @@ object Seven {
   fun main(args: Array<String>) {
     val input = "inputSeven.txt".readFileAsAList()
     problemOne(input)
+    problemTwo(input)
   }
 
   private fun problemOne(input: List<String>){
@@ -16,15 +17,26 @@ object Seven {
     input
       .map { it.toHand() }
       .sorted()
-      .foldIndexed(0){ i, acc , curr-> acc + curr.bidAmount * (i + 1) }
-      .let { println(it) }
+      .foldIndexed(0){ i, acc , curr-> acc + curr.bid * (i + 1) }
+      .let { println("solution p1: $it") }
     println("Time p1: ${System.currentTimeMillis() - time} ms")
   }
 
+  private fun problemTwo(input: List<String>){
+    val time = System.currentTimeMillis()
+    input
+      .map { it.toHand2() }
+      .sorted()
+      .foldIndexed(0){ i, acc , curr-> acc + curr.bid * (i + 1) }
+      .let { println("solution p2: $it") }
+    println("Time p2: ${System.currentTimeMillis() - time} ms")
+  }
+
   private val cardsMap = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+  private val cardsMap2 = listOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J') //index 12 is J(oker)
 
   private fun String.toHand(): Hand {
-    val (cards, bidAmount) = this.split(" ")
+    val (cards, bid) = this.split(" ")
     val cardsInt = cards.map { cardsMap.indexOf(it) }.toIntArray()
     val cardsValues = cards.groupingBy { it }.eachCount().toList().sortedByDescending { it.second }
     val categoryStrength = when {
@@ -36,10 +48,30 @@ object Seven {
       cardsValues.size == 4 && cardsValues[0].second == 2 -> 2
       else -> 1
     }
-    return Hand(categoryStrength, bidAmount.toShort(), cardsInt)
+    return Hand(cards, categoryStrength, bid.toShort(), cardsInt)
   }
 
-  private class Hand(val categoryStrength: Int, val bidAmount: Short, val intValues : IntArray): Comparable<Hand> {
+  private fun String.toHand2(): Hand {
+    val (cards, bid) = this.split(" ")
+    val cardsInt = cards.map { cardsMap2.indexOf(it) }
+    val categoryStrength = (0..< 12).maxOf { maybeJoker ->
+      val cardIntJokerized = cardsInt.map { if (it == 12) maybeJoker else it }
+      val cardsMap = cardIntJokerized.groupingBy { it }.eachCount().toList().sortedByDescending { it.second }
+      when {
+        cardsMap.size == 1 -> 7
+        cardsMap.size == 2 && cardsMap[0].second == 4 -> 6
+        cardsMap.size == 2 && cardsMap[0].second == 3 -> 5
+        cardsMap.size == 3 && cardsMap[0].second == 3 -> 4
+        cardsMap.size == 3 && cardsMap[0].second == 2 && cardsMap[1].second == 2 -> 3
+        cardsMap.size == 4 && cardsMap[0].second == 2 -> 2
+        else -> 1
+      }
+    }
+    return Hand(cards, categoryStrength, bid.toShort(), cardsInt.toIntArray())
+  }
+
+  //I left cards here for debug purposes, but it can be removed
+  private class Hand(val cards:String, val categoryStrength: Int, val bid: Short, val intValues : IntArray): Comparable<Hand> {
     override fun compareTo(other: Hand): Int {
       if (categoryStrength < other.categoryStrength) return -1
       if (categoryStrength > other.categoryStrength) return 1
